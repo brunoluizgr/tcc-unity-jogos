0d8a5169b273304c33deefa07c8ff912e397a45b
brunoluizgr <brunoluizgr@gmail.com>
2015-12-13 02:41:38 -0200

    Primeiro commit.

---
+++ TCC- Jogos Digitais/Assets/MenuPrincipal/Scripts/MenuPrincipal.cs
@@ -0,0 +1,12 @@
+﻿/// <summary>
+/// MenuPrincipal
+/// Anexado a Câmera Principal
+/// </summary>
+
+using UnityEngine;
+using System.Collections;
+
+public class MenuPrincipal : MonoBehaviour {
+
+    public Texture backgroundTextura;
+}
